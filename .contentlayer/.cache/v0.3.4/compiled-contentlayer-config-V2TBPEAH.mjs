d1470960da7f727ac6e9ca53eae2e702d479365e
Sanjay Roberts <sanjayroberts1@gmail.com>
2025-07-31 17:20:06 -0600

    fix dev

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-V2TBPEAH.mjs
@@ -0,0 +1,27 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+var Blog = defineDocumentType(() => ({
+  name: "Blog",
+  filePathPattern: `blog/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    date: { type: "date", required: true },
+    description: { type: "string", required: false }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, "")
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "content",
+  documentTypes: [Blog]
+});
+export {
+  Blog,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-V2TBPEAH.mjs.map
